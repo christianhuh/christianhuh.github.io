b4171ad3c482462e2943b995aef5affcd2ae4348
christianhuh <ch3110@nyu.edu>
2020-11-11 11:51:51 -0500

    work

---
+++ McLuhan_Response/mcluhan.js
@@ -0,0 +1,19 @@
+function quotes1(){
+    var x = document.getElementById("text");
+        x.innerHTML="Real, total war has become information war. It is being fought by subtle electric informational media — under cold conditions, and constantly."
+}
+
+function quotes2(){
+    var x = document.getElementById("text");
+        x.innerHTML="It is impossible to understand social and cultural changes without a knowledge of the workings of media."
+}
+
+function quotes3(){
+    var x = document.getElementById("text");
+        x.innerHTML="All media work us over completely."
+}
+
+function quotes4(){
+    var x = document.getElementById("text");
+        x.innerHTML="All media are extensions of some human faculty-psychic or physical."
+}
